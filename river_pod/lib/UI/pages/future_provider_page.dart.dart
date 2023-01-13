7183b32eeb5651951379e4ec63109dbc801d8a32
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2023-01-13 17:37:59 +0100

    💡 learning: Future Provider

--- river_pod/lib/UI/pages/future_provider_page.dart.dart
+++ river_pod/lib/UI/pages/future_provider_page.dart.dart
@@ -1,57 +1,57 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:river_pod/models/suggestion.dart';
-// import 'package:river_pod/services/api_service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:river_pod/models/suggestion.dart';
+import 'package:river_pod/services/api_service.dart';
 
-// //Create FutureProvider to read the ApiService Provider - autoDispose triggers the ApiServiceProvder to bring new Data
-// final suggestionFutureProvider = FutureProvider<Suggestion>((ref) {
-//   final apiService = ref.watch(apiServiceProvider);
-//   return apiService.getSuggestion();
-// });
+//Create FutureProvider to read the ApiService Provider - autoDispose triggers the ApiServiceProvder to bring new Data
+final suggestionFutureProvider = FutureProvider<Suggestion>((ref) {
+  final apiService = ref.watch(apiServiceProvider);
+  return apiService.getSuggestion();
+});
 
-// //final suggestionFutureProvider =
-// class FutureProviderPage extends ConsumerWidget {
-//   const FutureProviderPage({this.color, super.key});
-//   final Color? color;
-//   @override
-//   Widget build(BuildContext context, ref) {
-//     //watching the exposed by a provider and rebuild the widget when that value changes.
-//     final suggestionReference = ref.watch(suggestionFutureProvider);
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: color,
-//         title: const Text("Future Provider"),
-//         centerTitle: true,
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(10.0),
-//         child: RefreshIndicator(
-//           onRefresh: () {
-//             return ref.refresh(suggestionFutureProvider.future);
-//           },
-//           child: ListView(
-//             children: [
-//               Center(
-//                 //handle the FutureProvide changes with .when
-//                 child: suggestionReference.when(data: ((data) {
-//                   return Text(
-//                     data.activity!,
-//                     style: Theme.of(context).textTheme.headline5,
-//                   );
-//                 }), error: ((error, _) {
-//                   return Text(
-//                     error.toString(),
-//                     style: Theme.of(context).textTheme.headline5,
-//                     textAlign: TextAlign.center,
-//                   );
-//                 }), loading: () {
-//                   return const CircularProgressIndicator();
-//                 }),
-//               )
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+//final suggestionFutureProvider =
+class FutureProviderPage extends ConsumerWidget {
+  const FutureProviderPage({this.color, super.key});
+  final Color? color;
+  @override
+  Widget build(BuildContext context, ref) {
+    //watching the exposed by a provider and rebuild the widget when that value changes.
+    final suggestionReference = ref.watch(suggestionFutureProvider);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: color,
+        title: const Text("Future Provider"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: RefreshIndicator(
+          onRefresh: () {
+            return ref.refresh(suggestionFutureProvider.future);
+          },
+          child: ListView(
+            children: [
+              Center(
+                //handle the FutureProvide changes with .when
+                child: suggestionReference.when(data: ((data) {
+                  return Text(
+                    data.activity!,
+                    style: Theme.of(context).textTheme.headline5,
+                  );
+                }), error: ((error, _) {
+                  return Text(
+                    error.toString(),
+                    style: Theme.of(context).textTheme.headline5,
+                    textAlign: TextAlign.center,
+                  );
+                }), loading: () {
+                  return const CircularProgressIndicator();
+                }),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
