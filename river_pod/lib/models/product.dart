8238eb47920fa0da29b35d5b22290dfbee8172ca
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2023-01-16 03:36:20 +0100

    💡 learning: Change Notifier Provider

---
+++ river_pod/lib/models/product.dart
@@ -0,0 +1,42 @@
+//Product Model
+class Product {
+  Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+  });
+
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+}
+
+//Product lIST holding Product
+final List<Product> productList = [
+  Product(
+    id: "1",
+    title: "PS 5",
+    description: "Brand new PS5 Console",
+    price: 350.0,
+  ),
+  Product(
+    id: "2",
+    title: "M1 Pro",
+    description: "Apple product",
+    price: 650.0,
+  ),
+  Product(
+    id: "3",
+    title: "Crocs",
+    description: "Custom built crocs",
+    price: 75.50,
+  ),
+  Product(
+    id: "4",
+    title: "Hoodie",
+    description: "AOT Merch Hoodie",
+    price: 19.99,
+  )
+];
---
+++ river_pod/lib/services/cart_notifier.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:river_pod/models/product.dart';
+
+//provider to extend cart Notifier using ChangeNotifierProvider
+final cartNotifierProvider = ChangeNotifierProvider<CartNotifier>((ref) {
+  return CartNotifier();
+});
+
+class CartNotifier extends ChangeNotifier {
+  //Empty list that hold the Product when any of the methods are called
+  List<Product> _cart = [];
+
+  get cart => _cart;
+
+  void addProduct(Product product) {
+    //print(_cart);
+    _cart.add(product);
+    notifyListeners();
+  }
+
+  void removeProduct(Product product) {
+    _cart.remove(product);
+    notifyListeners();
+  }
+
+  void clear() {
+    _cart.clear();
+    notifyListeners();
+  }
+}
