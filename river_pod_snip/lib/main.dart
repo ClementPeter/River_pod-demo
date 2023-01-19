bde0550a28058072c559e00b9deb3a724f7dd63d
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2023-01-19 17:26:19 +0100

    ⛏ Building: Projects_Riverpod

--- river_pod_snip/lib/main.dart
+++ river_pod_snip/lib/main.dart
@@ -1,7 +1,81 @@
+// import 'package:flutter/material.dart';
+// import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+// //Building 6 MINI projects with Riverpod and Hooks Riverpod
+
+// void main() {
+//   runApp(
+//     ProviderScope(
+//       child: const MyApp(),
+//     ),
+//   );
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Riverpod projects',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const MyHomePage(),
+//     );
+//   }
+// }
+
+// final currentDateProvider = Provider<DateTime>((ref) {
+//   return DateTime.now();
+// });
+
+// class MyHomePage extends ConsumerWidget {
+//   const MyHomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     final currentDate = ref.watch(currentDateProvider);
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("Hooks Riverpod projects"),
+//         centerTitle: true,
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Text(
+//               currentDate.toIso8601String(),
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//             //button
+//             ElevatedButton(
+//               onPressed: (() {
+//                 ref.refresh(currentDateProvider);
+//               }),
+//               child: const Text("Refresh"),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+//DEMO APP
+
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+//Building 6 MINI projects with Riverpod and Hooks Riverpod
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -10,57 +84,85 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Riverpod projects',
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+// //extension called OptionalInFixAddition to add null and non-null values
+// extension OptionalInfixAddition<T extends num> on T? {
+//   T? operator +(T? other) {
+//     final shadow = this;
+//     if (shadow != null) {
+//       return shadow + (other ?? 0) as T;
+//     } else {
+//       return null;
+//     }
+//   }
+// }
 
-  final String title;
+//function to add a null and  non-null variable using extensions
+// void testIt() {
+//   final int? int1 = 1;
+//   final int? int2 = 1;
+//   final result = int1 + int2;
+//   print(result);
+// }
+//////////////////////////StateNotifier//////////////////////////
+class Counter extends StateNotifier {
+  Counter() : super(null);
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void increment() {
+    state = state == null ? 1 : state + 1;
   }
+}
+//StateNotifierProvider is the provider that allows us to use StateNotifier
+final counterProvider = StateNotifierProvider((ref) {
+  return Counter();
+});
+
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    //fina
+    // testIt();
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        //Wrap the text in Consumer widget prevents us from using the parent "ref" which rebuilds
+        //the entire scaffold when the counter value changes
+        title: Consumer(
+          builder: ((context, ref, child) {
+            final counter = ref.watch(counterProvider);
+            final text = counter == null ? "Press to begin" : "$counter";
+
+            return Text(text);
+          }),
+        ),
+        centerTitle: true,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            //text button
+            TextButton(
+              // onPressed: (() {
+              //   ref.read(counterProvider.notifier).increment();
+              // }),
+              onPressed: ref.read(counterProvider.notifier).increment,
+
+              child: const Text("Increment"),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
