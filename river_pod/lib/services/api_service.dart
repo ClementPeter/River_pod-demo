5c65ffaf87f444c8130a6bec4dd241eef6ec5ec1
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2023-01-19 18:15:23 +0100

    ✨ completed: Riverpod Demo

--- river_pod/lib/services/api_service.dart
+++ river_pod/lib/services/api_service.dart
@@ -1,24 +1,3 @@
-// import 'package:dio/dio.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:river_pod/models/suggestion.dart';
-
-// //Create a provider to provide ApiService as a class - Used to Demo FutureProvider in future_provider_page.dart
-// final apiServiceProvider = Provider<ApiService>(((ref) => ApiService()));
-
-// //Http request using Dio and returning model
-// class ApiService {
-//   Future<Suggestion> getSuggestion() async {
-//     try {
-//       var response = await Dio().get('http://www.boredapi.com/api/activity/');
-//       print(response);
-//       return Suggestion.fromJson(response.data);
-//     } catch (e) {
-//       throw Exception('Error getting suggestion');
-//     }
-//   }
-// }
-
-// Uncomment Demo usage of .family modifier to pass and append value to API
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:river_pod/models/suggestion.dart';
@@ -28,8 +7,7 @@ final apiServiceProvider = Provider<ApiService>(((ref) => ApiService()));
 
 //Http request using Dio and returning model
 class ApiService {
-  Future<Suggestion> getSuggestion(String id) async {
-    print(id);
+  Future<Suggestion> getSuggestion() async {
     try {
       var response = await Dio().get('http://www.boredapi.com/api/activity/');
       print(response);
@@ -39,3 +17,25 @@ class ApiService {
     }
   }
 }
+
+// // Uncomment Demo usage of .family modifier to pass and append value to API
+// import 'package:dio/dio.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:river_pod/models/suggestion.dart';
+
+// //Create a provider to provide ApiService as a class - Used to Demo FutureProvider in future_provider_page.dart
+// final apiServiceProvider = Provider<ApiService>(((ref) => ApiService()));
+
+// //Http request using Dio and returning model
+// class ApiService {
+//   Future<Suggestion> getSuggestion(String id) async {
+//     print(id);
+//     try {
+//       var response = await Dio().get('http://www.boredapi.com/api/activity/');
+//       print(response);
+//       return Suggestion.fromJson(response.data);
+//     } catch (e) {
+//       throw Exception('Error getting suggestion');
+//     }
+//   }
+// }
