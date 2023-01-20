5998193e4b20ecaa522753058a2374536153ab45
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2023-01-20 22:00:29 +0100

    ⛏ Building: Projects_Riverpod

--- river_pod_snip/lib/main.dart
+++ river_pod_snip/lib/main.dart
@@ -63,7 +63,116 @@
 //   }
 // }
 
-//DEMO APP
+// //DEMO APP 2 using StateNotiifereProvider
+
+// import 'package:flutter/material.dart';
+// import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+// //Building 6 MINI projects with Riverpod and Hooks Riverpod
+
+// void main() {
+//   runApp(
+//     const ProviderScope(
+//       child: MyApp(),
+//     ),
+//   );
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Riverpod projects',
+//       darkTheme: ThemeData.dark(),
+//       themeMode: ThemeMode.dark,
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const MyHomePage(),
+//     );
+//   }
+// }
+
+// // //extension called OptionalInFixAddition to add null and non-null values
+// // extension OptionalInfixAddition<T extends num> on T? {
+// //   T? operator +(T? other) {
+// //     final shadow = this;
+// //     if (shadow != null) {
+// //       return shadow + (other ?? 0) as T;
+// //     } else {
+// //       return null;
+// //     }
+// //   }
+// // }
+
+// //function to add a null and  non-null variable using extensions
+// // void testIt() {
+// //   final int? int1 = 1;
+// //   final int? int2 = 1;
+// //   final result = int1 + int2;
+// //   print(result);
+// // }
+// //////////////////////////StateNotifier//////////////////////////
+// class Counter extends StateNotifier {
+//   Counter() : super(null);
+
+//   void increment() {
+//     state = state == null ? 1 : state + 1;
+//   }
+// }
+// //StateNotifierProvider is the provider that allows us to use StateNotifier
+// final counterProvider = StateNotifierProvider((ref) {
+//   return Counter();
+// });
+
+// class MyHomePage extends ConsumerWidget {
+//   const MyHomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     //fina
+//     // testIt();
+//     return Scaffold(
+//       appBar: AppBar(
+//         //Wrap the text in Consumer widget prevents us from using the parent "ref" which rebuilds
+//         //the entire scaffold when the counter value changes
+//         title: Consumer(
+//           builder: ((context, ref, child) {
+//             final counter = ref.watch(counterProvider);
+//             final text = counter == null ? "Press to begin" : "$counter";
+
+//             return Text(text);
+//           }),
+//         ),
+//         centerTitle: true,
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             //text button
+//             TextButton(
+//               // onPressed: (() {
+//               //   ref.read(counterProvider.notifier).increment();
+//               // }),
+//               onPressed: ref.read(counterProvider.notifier).increment,
+
+//               child: const Text("Increment"),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+/////////DEMO APP 3 Weather App using FutureProvider and Mock Data
+/////DEMO APP 2 using StateNotiifereProvider
 
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -95,37 +204,37 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// //extension called OptionalInFixAddition to add null and non-null values
-// extension OptionalInfixAddition<T extends num> on T? {
-//   T? operator +(T? other) {
-//     final shadow = this;
-//     if (shadow != null) {
-//       return shadow + (other ?? 0) as T;
-//     } else {
-//       return null;
-//     }
+// // //extension called OptionalInFixAddition to add null and non-null values
+// // extension OptionalInfixAddition<T extends num> on T? {
+// //   T? operator +(T? other) {
+// //     final shadow = this;
+// //     if (shadow != null) {
+// //       return shadow + (other ?? 0) as T;
+// //     } else {
+// //       return null;
+// //     }
+// //   }
+// // }
+
+// //function to add a null and  non-null variable using extensions
+// // void testIt() {
+// //   final int? int1 = 1;
+// //   final int? int2 = 1;
+// //   final result = int1 + int2;
+// //   print(result);
+// // }
+// //////////////////////////StateNotifier//////////////////////////
+// class Counter extends StateNotifier {
+//   Counter() : super(null);
+
+//   void increment() {
+//     state = state == null ? 1 : state + 1;
 //   }
 // }
-
-//function to add a null and  non-null variable using extensions
-// void testIt() {
-//   final int? int1 = 1;
-//   final int? int2 = 1;
-//   final result = int1 + int2;
-//   print(result);
-// }
-//////////////////////////StateNotifier//////////////////////////
-class Counter extends StateNotifier {
-  Counter() : super(null);
-
-  void increment() {
-    state = state == null ? 1 : state + 1;
-  }
-}
-//StateNotifierProvider is the provider that allows us to use StateNotifier
-final counterProvider = StateNotifierProvider((ref) {
-  return Counter();
-});
+// //StateNotifierProvider is the provider that allows us to use StateNotifier
+// final counterProvider = StateNotifierProvider((ref) {
+//   return Counter();
+// });
 
 class MyHomePage extends ConsumerWidget {
   const MyHomePage({super.key});
@@ -138,14 +247,7 @@ class MyHomePage extends ConsumerWidget {
       appBar: AppBar(
         //Wrap the text in Consumer widget prevents us from using the parent "ref" which rebuilds
         //the entire scaffold when the counter value changes
-        title: Consumer(
-          builder: ((context, ref, child) {
-            final counter = ref.watch(counterProvider);
-            final text = counter == null ? "Press to begin" : "$counter";
-
-            return Text(text);
-          }),
-        ),
+        title:Text("Weather"),
         centerTitle: true,
       ),
       body: Center(
@@ -153,17 +255,11 @@ class MyHomePage extends ConsumerWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             //text button
-            TextButton(
-              // onPressed: (() {
-              //   ref.read(counterProvider.notifier).increment();
-              // }),
-              onPressed: ref.read(counterProvider.notifier).increment,
-
-              child: const Text("Increment"),
-            ),
+       
           ],
         ),
       ),
     );
   }
 }
+
